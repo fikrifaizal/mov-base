621a48b470f0661bf16d000c1d3a0fb505437e72
fikrifaizal <fikrifaizal40@gmail.com>
2023-04-12 11:26:24 +0700

    [Add] create dependency injection for view model

---
+++ app/src/main/java/com/sinau/movbase/di/AppModule.kt
@@ -0,0 +1,19 @@
+package com.sinau.movbase.di
+
+import com.sinau.movbase.core.domain.usecase.MovieInteractor
+import com.sinau.movbase.core.domain.usecase.MovieUseCase
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
+import dagger.hilt.android.scopes.ViewModelScoped
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(ViewModelComponent::class)
+abstract class AppModule {
+
+    @Binds
+    @ViewModelScoped
+    abstract fun provideMovieUseCase(movieInteractor: MovieInteractor): MovieUseCase
+}
